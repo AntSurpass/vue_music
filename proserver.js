574ab4af5e96451a708bf8032eb3f2047d88dd08
huyuechao <280812241@qq.com>
2017-11-10 11:24:55 +0800

    完整提交

---
+++ proserver.js
@@ -0,0 +1,71 @@
+var express = require('express')
+var axios = require('axios')
+
+const app = express()
+
+var apiRoutes = express.Router()
+
+apiRoutes.get('/getDiscList', function (req, res) {
+  var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
+apiRoutes.get('/getSongList', function (req, res) {
+  var url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
+apiRoutes.get('/lyric', function (req, res) {
+  var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    let ret = response.data
+    if (typeof ret === "string") {
+      let reg = /^\w+\(({[^()]+})\)$/
+      let matchs = ret.match(reg)
+      if (matchs) {
+        ret = JSON.parse(matchs[1])
+      }
+    }
+    res.json(ret)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
+app.use('/api', apiRoutes)
+
+app.use(express.static('./music'))
+
+var prot = 9000
+module.express = app.listen(prot, function (err) {
+  if (err) {
+    console.log(err)
+  }
+  console.log('listening at http://localhost:' + prot + '\n')
+})
